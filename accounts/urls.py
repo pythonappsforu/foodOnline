3da969573beb58c15cf390c515f6e603ee277246
pythonbyfebash <pythonbyfebash@gmail.com>
2024-01-09 21:45:08 +0530

    customer and vendor registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,10 @@ from . import views
 urlpatterns = [
     path('register/', views.registerUser, name='registerUser'),
     path('registerVendor/',views.registerVendor, name='registerVendor'),
+
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout,name='logout'),
+    path('myAccount/', views.myAccount, name='myAccount'),
+    path('vendorDashboard/', views.vendorDashboard, name='vendorDashboard'),
+    path('custDashboard/', views.custDashboard, name='custDashboard'),
 ]
