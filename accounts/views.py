8643d15b09632975c881686164df809fa8e6a9b7
pythonbyfebash <pythonbyfebash@gmail.com>
2024-01-24 04:53:35 +0530

    cart functionality

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.tokens import default_token_generator
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
+from django.template.defaultfilters import slugify
 from django.utils.http import urlsafe_base64_decode
 
 from vendor.forms import VendorForm
@@ -95,6 +96,8 @@ def registerVendor(request):
             user.save()
             vendor = vendor_form.save(commit=False)
             vendor.user = user
+            vendor_name = vendor_form.cleaned_data['vendor_name']
+            vendor.vendor_slug = slugify(vendor_name) + '-' + str(user.id)
             user_profile = UserProfile.objects.get(user=user)
             vendor.user_profile = user_profile
             vendor.save()
