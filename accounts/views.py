b046a4d2a830f7406337f8c86a60279c78ca6e7a
pythonbyfebash <pythonbyfebash@gmail.com>
2024-02-10 22:34:40 +0530

    orders and order detail functionalities for vendor/customer

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.tokens import default_token_generator
 from django.core.exceptions import PermissionDenied
@@ -195,11 +197,30 @@ def custDashboard(request):
 @user_passes_test(check_role_vendor)
 def vendorDashboard(request):
     vendor = Vendor.objects.get(user=request.user)
-    context = {
-        'vendor':vendor,
-    }
-    return render(request,'accounts/vendorDashboard.html',context)
+    orders = Order.objects.filter(vendors__in=[vendor.id],
+                                  is_ordered=True).order_by('created_at')
+    recent_orders = orders[:10]
 
+    # current month's revenue
+    current_month = datetime.datetime.now().month
+    current_month_orders = orders.filter(vendors__in=[vendor.id],
+                                         created_at__month=current_month)
+    current_month_revenue = 0
+    for i in current_month_orders:
+        current_month_revenue += i.get_total_by_vendor()['grand_total']
+
+    # total revenue
+    total_revenue = 0
+    for i in orders:
+        total_revenue += i.get_total_by_vendor()['grand_total']
+    context = {
+        'orders': orders,
+        'orders_count': orders.count(),
+        'recent_orders': recent_orders,
+        'total_revenue': total_revenue,
+        'current_month_revenue': current_month_revenue,
+    }
+    return render(request, 'accounts/vendorDashboard.html', context)
 
 def forgot_password(request):
     if request.method == 'POST':
